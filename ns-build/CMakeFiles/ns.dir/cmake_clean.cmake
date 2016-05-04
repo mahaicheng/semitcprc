ca2aecd8a18187ccf51a62560f369c86bd009319
Ma Haicheng <hchma@outlook.com>
2016-05-04 13:36:30 +0800

    initialized

---
+++ ns-build/CMakeFiles/ns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ns.dir/main.cpp.o"
+  "ns.pdb"
+  "ns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
